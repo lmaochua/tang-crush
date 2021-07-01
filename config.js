9ff5932291607ad6965d55e0b2e271bf42b36e79
lmaochua <86754001+lmaochua@users.noreply.github.com>
2021-07-01 08:30:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'He luu cậu!!',
+    introDesc: 'tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaaa',
+    btnIntro: 'Ok',
+    title: 'Cậu yêu tớ có phải không nào',
+    desc: 'nếu cậu không trả lời mà thoát ra tức là muốn làm vợ tớ rồi đó nha :v ',
+    btnYes: 'Yêu ơi là yêuuu <3333',
+    btnNo: 'Cậu mơ à :3333',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://m.me/QING1750'
